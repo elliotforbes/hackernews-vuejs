ce2986a6082a8b0e2c34e37ac696f13df0208a1f
Elliot Forbes <emforce@live.com>
2019-02-11 22:25:27 +0000

    Updated to vue cli 3

--- src/store/mutations.js
+++
@@ -1,8 +0,0 @@
-export default {
-  APPEND_TOP_STORY: (state, article) => {
-    state.topStories.push(article)
-  },
-  APPEND_NEW_STORY: (state, article) => {
-    state.newStories.push(article)
-  }
-}
